85d78c3676bcf69d5e2aed349f274589372d5015
sooyeoniya <tndus1306403@naver.com>
2024-12-13 17:54:40 +0900

    refactor(constants): 메뉴 타입 상수 통일

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -24,18 +24,18 @@ export const MENU_TYPE = Object.freeze({
 })
 
 export const MENUS = Object.freeze([
-  { type: 'appetizer', name: '양송이수프', price: 6_000 },
-  { type: 'appetizer', name: '타파스', price: 5_500 },
-  { type: 'appetizer', name: '시저샐러드', price: 8_000 },
-  { type: 'main', name: '티본스테이크', price: 55_000 },
-  { type: 'main', name: '바비큐립', price: 54_000 },
-  { type: 'main', name: '해산물파스타', price: 35_000 },
-  { type: 'main', name: '크리스마스파스타', price: 25_000 },
-  { type: 'dessert', name: '초코케이크', price: 15_000 },
-  { type: 'dessert', name: '아이스크림', price: 5_000 },
-  { type: 'drink', name: '제로콜라', price: 3_000 },
-  { type: 'drink', name: '레드와인', price: 60_000 },
-  { type: 'drink', name: '샴페인', price: 25_000 },
+  { type: MENU_TYPE.APPETIZER, name: '양송이수프', price: 6_000 },
+  { type: MENU_TYPE.APPETIZER, name: '타파스', price: 5_500 },
+  { type: MENU_TYPE.APPETIZER, name: '시저샐러드', price: 8_000 },
+  { type: MENU_TYPE.MAIN, name: '티본스테이크', price: 55_000 },
+  { type: MENU_TYPE.MAIN, name: '바비큐립', price: 54_000 },
+  { type: MENU_TYPE.MAIN, name: '해산물파스타', price: 35_000 },
+  { type: MENU_TYPE.MAIN, name: '크리스마스파스타', price: 25_000 },
+  { type: MENU_TYPE.DESSERT, name: '초코케이크', price: 15_000 },
+  { type: MENU_TYPE.DESSERT, name: '아이스크림', price: 5_000 },
+  { type: MENU_TYPE.DRINK, name: '제로콜라', price: 3_000 },
+  { type: MENU_TYPE.DRINK, name: '레드와인', price: 60_000 },
+  { type: MENU_TYPE.DRINK, name: '샴페인', price: 25_000 },
 ]);
 
 export const ERROR_MESSAGES = Object.freeze({
